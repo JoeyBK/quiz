4199dd0e648ac7cd6eb3fcafe7e30134f29c028e
Joey Azoulai <joey@spotbrooklyn.com>
2014-05-15 12:12:18 -0400

    rewrite quiz using Parasitic Combination Inheritance Pattern.

--- quizShow.js
+++ quizShow.js
@@ -1,7 +1,61 @@
 /**
  * Created by jazoulai on 5/8/14.
  */
-var Questions = [
+
+function inheritPrototype(childObject, parentObject) {
+    // As discussed above, we use the Crockford’s method to copy the properties and methods from the parentObject onto the childObject
+// So the copyOfParent object now has everything the parentObject has
+    var copyOfParent = Object.create(parentObject.prototype);
+
+    //Then we set the constructor of this new object to point to the childObject.
+// Why do we manually set the copyOfParent constructor here, see the explanation immediately following this code block.
+    copyOfParent.constructor = childObject;
+
+    // Then we set the childObject prototype to copyOfParent, so that the childObject can in turn inherit everything from copyOfParent (from parentObject)
+    childObject.prototype = copyOfParent;
+}
+
+function Question(theQuestion, theChoices, theCorrectAnswer){
+    this.question = theQuestion;
+    this.choices = theChoices;
+    this.correctAnswer = theCorrectAnswer;
+    this.userAnswer = '';
+}
+
+Question.prototype.getCorrectAnswer = function() {
+    return this.correctAnswer;
+};
+
+Question.prototype.getUserAnswer = function(){
+    return this.userAnswer;
+};
+
+Question.prototype.displayQuestion = function(){
+
+    var questionToDisplay = '<div class="question">' + this.question + '</div><ul>';
+    choiceCounter = 0;
+
+    this.choices.forEach(function(eachChoice){
+        questionToDisplay += '<li><input type="radio" name="choice" value="' + choiceCounter + '">' + eachChoice + '</li>';
+        choiceCounter++
+    });
+
+    questionToDisplay += '</ul>';
+
+    var quizDiv = document.getElementById('quiz');
+
+    quizDiv.innerHTML = questionToDisplay;
+};
+
+function MultipleChoiceQuestion(theQuestion, theChoices, theCorrectAnswer){
+    Question.call(this, theQuestion, theChoices, theCorrectAnswer);
+};
+
+inheritPrototype(MultipleChoiceQuestion, Question);
+
+
+
+var allQuestions = [
     {
         question: "What city where you born in?",
         choices: [
@@ -34,72 +88,28 @@ var Questions = [
     }
 ];
 
-var questionDiv = document.getElementById("question");
-var choiceDiv = document.getElementById("choices");
-var button = document.getElementById('next');
-var form = document.getElementById('choices');
-var radios = document.getElementsByName('multi');
-var score = 0;
-var scoreDiv = document.getElementById('score');
+// Display all the questions
+
+
 var i = -1;
 
-scoreDiv.innerHTML = 'score: ' + score;
-
 function next() {
-    if (i < Questions.length - 1) {
 
+    if (i < allQuestions.length) {
         i++;
+        var testQ = new MultipleChoiceQuestion(allQuestions[i].question, allQuestions[i].choices, allQuestions[i].correctAnswer);
+        testQ.displayQuestion();
 
-        questionDiv.innerHTML = Questions[i].question;
-        var choice = Questions[i].choices;
-
-        choiceDiv.innerHTML = '';
-
-
-        choice.forEach(function(e){
-            var listItem = document.createElement('input');
-            var label = document.createElement('label');
-            var brake = document.createElement('br');
-            listItem.setAttribute('type', 'radio');
-            listItem.setAttribute('name', 'multi');
-            listItem.setAttribute('id', e);
-            label.setAttribute('for', e);
-            label.innerHTML = e;
-            choiceDiv.appendChild(listItem);
-            choiceDiv.appendChild(label);
-            choiceDiv.appendChild(brake);
-        });
     }
 }
-
-
-var j = 0;
-function answer(){
-    var correct = Questions[j].correctAnswer;
-
-       if(j < Questions.length){
-           j++;
-        }
-
-    for(var i = 0; i < radios.length; i++){
-        if(radios[i].checked){
-            if(radios[i].id == correct){
-                score += 5;
-                scoreDiv.innerHTML = 'score: ' + score;
-            } else {
-                console.log('try again!');
-            }
-
-        }
-    }
-}
-
-
 
 next();
 
+var button = document.getElementById('next');
+
 button.onclick = function () {
-    answer();
     next();
 };
+
+
 
