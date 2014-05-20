3dc28b1b36416f08e93f32bb2ea1203f6de2e99d
Joey Azoulai <joey@spotbrooklyn.com>
2014-05-20 18:18:19 -0400

    add and test Jquery.

--- quizShow.js
+++ quizShow.js
@@ -1,112 +1,118 @@
 //define questions
 var allQuestions = [
     {
-        question    : "Before it was named JavaScript, what was the language called?",
-        choices     : ["TypeScript", "Java", "JScript", "LiveScript"],
-        correctAnswer      : 3
+        question: "Before it was named JavaScript, what was the language called?",
+        choices: ["TypeScript", "Java", "JScript", "LiveScript"],
+        correctAnswer: 3
     },
     {
-        question    : "Who did originally develop JavaScript?",
-        choices     : ["Bill Gates", "Brendan Eich", "Douglas CrockFord", "Nicholas Zakas", "Steve Jobs"],
-        correctAnswer      : 1
+        question: "Who did originally develop JavaScript?",
+        choices: ["Bill Gates", "Brendan Eich", "Douglas CrockFord", "Nicholas Zakas", "Steve Jobs"],
+        correctAnswer: 1
     },
     {
-        question    : "Which browser was the first to implement JavaScript?",
-        choices     : ["Internet Explorer", "Netscape Navigator", "Opera"],
-        correctAnswer      : 1
+        question: "Which browser was the first to implement JavaScript?",
+        choices: ["Internet Explorer", "Netscape Navigator", "Opera"],
+        correctAnswer: 1
     },
     {
-        question    : "Microsoft called JavaScript something else to avoid trademark issues. What was that name?",
-        choices     : ["MicroScript", "IEScript", "VBScript", "JScript", "BasicScript"],
-        correctAnswer      : 3
+        question: "Microsoft called JavaScript something else to avoid trademark issues. What was that name?",
+        choices: ["MicroScript", "IEScript", "VBScript", "JScript", "BasicScript"],
+        correctAnswer: 3
     }
 ];
 
+$(document).ready(function(){
 
+    var quizEngine = {};
+    quizEngine.currentQuestionIndex = 0;
+    quizEngine.score = 0;
+    quizEngine.questionBox = $('#question');
+    quizEngine.answersBox = document.getElementById("answers");
+    quizEngine.scoreBox = document.getElementById("score");
+    quizEngine.button = document.getElementById("button");
 
+    quizEngine.run_quiz = function () {
+        if (this.currentQuestionIndex < allQuestions.length) {
+            this.clear_question();
+            this.print_question();
+            this.print_answers();
+        } else {
+            this.compute_score();
+        }
+    };
 
-
-var quizEngine = {};
-quizEngine.currentQuestionIndex = 0;
-quizEngine.score = 0;
-quizEngine.questionBox = document.getElementById("question");
-quizEngine.answersBox = document.getElementById("answers");
-quizEngine.scoreBox = document.getElementById("score");
-quizEngine.button = document.getElementById("button");
-
-quizEngine.run_quiz = function(){
-    if(this.currentQuestionIndex < allQuestions.length){
-        this.clear_question();
-        this.print_question();
-        this.print_answers();
-    }else{
-        this.compute_score();
-    }
+    quizEngine.print_question = function () {
+        var currentQuestion = allQuestions[this.currentQuestionIndex].question;
+        quizEngine.questionBox.html("<p>" + currentQuestion + "</p>");
 };
 
-quizEngine.print_question = function(){
-        quizEngine.questionBox.innerHTML = allQuestions[this.currentQuestionIndex].question;
-};
+    quizEngine.print_answers = function () {
+        var choice = allQuestions[this.currentQuestionIndex].choices;
+        choice.forEach(function (e) {
 
-quizEngine.print_answers = function(){
-  var choice = allQuestions[this.currentQuestionIndex].choices;
-    choice.forEach(function(e){
-        var listItem = document.createElement('input');
-        var label = document.createElement('label');
-        var brake = document.createElement('br');
-        listItem.setAttribute('type', 'radio');
-        listItem.setAttribute('name', 'currentAnswer');
-        listItem.setAttribute('class', "radioAnswer");
-        listItem.setAttribute('value', e);
-        label.setAttribute('for', e);
-        label.innerHTML = e;
-        quizEngine.answersBox.appendChild(listItem);
-        quizEngine.answersBox.appendChild(label);
-        quizEngine.answersBox.appendChild(brake);
-    });
+            /*var currentChoices = '<input type="radio" name="currentAnswer" class="radioAnswer" value="' + e + '">' + '<label for="'+e+'">'+e+'</label><br>';
 
-};
+            quizEngine.answersBox.append(currentChoices);*/
 
-quizEngine.compute_score = function(){
-    quizEngine.clear_question();
-    quizEngine.scoreBox.innerHTML = "Your score: " + quizEngine.score;
-};
 
-quizEngine.next_question = function(){
-    quizEngine.check_answer();
-    quizEngine.currentQuestionIndex ++;
-    quizEngine.run_quiz();
-};
+            var listItem = document.createElement('input');
+            var label = document.createElement('label');
+            var brake = document.createElement('br');
+            listItem.setAttribute('type', 'radio');
+            listItem.setAttribute('name', 'currentAnswer');
+            listItem.setAttribute('class', "radioAnswer");
+            listItem.setAttribute('value', e);
+            label.setAttribute('for', e);
+            label.innerHTML = e;
+            quizEngine.answersBox.appendChild(listItem);
+            quizEngine.answersBox.appendChild(label);
+            quizEngine.answersBox.appendChild(brake);
+        });
 
-quizEngine.clear_question = function(){
-    quizEngine.questionBox.innerHTML = '';
-    quizEngine.answersBox.innerHTML = '';
-};
+    };
 
-quizEngine.check_answer = function(){
-    var radios = quizEngine.answersBox.getElementsByTagName('input');
-    for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
+    quizEngine.compute_score = function () {
+        quizEngine.clear_question();
+        quizEngine.scoreBox.innerHTML = "Your score: " + quizEngine.score;
+    };
 
-            var value = radios[i].value;
+    quizEngine.next_question = function () {
+        quizEngine.check_answer();
+        quizEngine.currentQuestionIndex++;
+        quizEngine.run_quiz();
+    };
 
-            var correct_answer_index = allQuestions[quizEngine.currentQuestionIndex].correctAnswer;
-            var currentCorrectAnswer = allQuestions[quizEngine.currentQuestionIndex].choices[correct_answer_index];
+    quizEngine.clear_question = function () {
+        quizEngine.questionBox.children().remove();
+        quizEngine.answersBox.innerHTML = '';
+    };
 
-            if(value == currentCorrectAnswer){ //if correct increment score
-                quizEngine.score ++;
-            }else{
-                console.log("wrong answer");
+    quizEngine.check_answer = function () {
+        var radios = quizEngine.answersBox.getElementsByTagName('input');
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+
+                var value = radios[i].value;
+
+                var correct_answer_index = allQuestions[quizEngine.currentQuestionIndex].correctAnswer;
+                var currentCorrectAnswer = allQuestions[quizEngine.currentQuestionIndex].choices[correct_answer_index];
+
+                if (value == currentCorrectAnswer) {
+                    quizEngine.score++;
+                } else {
+                    console.log("wrong answer");
+                }
             }
         }
-    }
-};
+    };
 
-quizEngine.run_quiz();
-
+    quizEngine.run_quiz();
 
 
-quizEngine.button.onclick = function(){
-    quizEngine.next_question();
+    quizEngine.button.onclick = function () {
+        quizEngine.next_question();
 
-};
+    };
+
+});
