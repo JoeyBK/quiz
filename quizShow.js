39a4eea76fcb64b40445b39808097af36496fdf8
Joey Azoulai <joey@spotbrooklyn.com>
2014-05-20 20:02:12 -0400

    refactor using jQuery.

--- quizShow.js
+++ quizShow.js
@@ -27,10 +27,8 @@ $(document).ready(function(){
     var quizEngine = {};
     quizEngine.currentQuestionIndex = 0;
     quizEngine.score = 0;
-    quizEngine.questionBox = $('#question');
-    quizEngine.answersBox = document.getElementById("answers");
-    quizEngine.scoreBox = document.getElementById("score");
-    quizEngine.button = document.getElementById("button");
+    quizEngine.questionDiv = $('#question');
+    quizEngine.answersDiv = $('#answers');
 
     quizEngine.run_quiz = function () {
         if (this.currentQuestionIndex < allQuestions.length) {
@@ -44,37 +42,28 @@ $(document).ready(function(){
 
     quizEngine.print_question = function () {
         var currentQuestion = allQuestions[this.currentQuestionIndex].question;
-        quizEngine.questionBox.html("<p>" + currentQuestion + "</p>");
-};
+        var currentQuestionHtml = '<p>' + currentQuestion + '</p>';
+        quizEngine.questionDiv.html(currentQuestionHtml);
+    };
 
     quizEngine.print_answers = function () {
-        var choice = allQuestions[this.currentQuestionIndex].choices;
-        choice.forEach(function (e) {
-
-            /*var currentChoices = '<input type="radio" name="currentAnswer" class="radioAnswer" value="' + e + '">' + '<label for="'+e+'">'+e+'</label><br>';
-
-            quizEngine.answersBox.append(currentChoices);*/
-
-
-            var listItem = document.createElement('input');
-            var label = document.createElement('label');
-            var brake = document.createElement('br');
-            listItem.setAttribute('type', 'radio');
-            listItem.setAttribute('name', 'currentAnswer');
-            listItem.setAttribute('class', "radioAnswer");
-            listItem.setAttribute('value', e);
-            label.setAttribute('for', e);
-            label.innerHTML = e;
-            quizEngine.answersBox.appendChild(listItem);
-            quizEngine.answersBox.appendChild(label);
-            quizEngine.answersBox.appendChild(brake);
+        var currentChoices = allQuestions[this.currentQuestionIndex].choices;
+        currentChoices.forEach(function (e) {
+            var currentChoicesHtml = '';
+            currentChoicesHtml += '<input type="radio"';
+            currentChoicesHtml += 'name="currentAnswer"';
+            currentChoicesHtml += 'class="radioAnswer"';
+            currentChoicesHtml += 'value="' + e + '">';
+            currentChoicesHtml += '<label for="'+e+'">';
+            currentChoicesHtml += e;
+            currentChoicesHtml += '</label><br>';
+            quizEngine.answersDiv.append(currentChoicesHtml);
         });
-
     };
 
     quizEngine.compute_score = function () {
         quizEngine.clear_question();
-        quizEngine.scoreBox.innerHTML = "Your score: " + quizEngine.score;
+        $('#score').html("Your score: " + quizEngine.score);
     };
 
     quizEngine.next_question = function () {
@@ -84,20 +73,17 @@ $(document).ready(function(){
     };
 
     quizEngine.clear_question = function () {
-        quizEngine.questionBox.children().remove();
-        quizEngine.answersBox.innerHTML = '';
+        quizEngine.questionDiv.children().remove();
+        quizEngine.answersDiv.children().remove();
     };
 
     quizEngine.check_answer = function () {
-        var radios = quizEngine.answersBox.getElementsByTagName('input');
+        var radios = quizEngine.answersDiv.find('input');
         for (var i = 0; i < radios.length; i++) {
             if (radios[i].checked) {
-
                 var value = radios[i].value;
-
                 var correct_answer_index = allQuestions[quizEngine.currentQuestionIndex].correctAnswer;
                 var currentCorrectAnswer = allQuestions[quizEngine.currentQuestionIndex].choices[correct_answer_index];
-
                 if (value == currentCorrectAnswer) {
                     quizEngine.score++;
                 } else {
@@ -109,10 +95,8 @@ $(document).ready(function(){
 
     quizEngine.run_quiz();
 
-
-    quizEngine.button.onclick = function () {
+    $("#button").on('click', function(){
         quizEngine.next_question();
-
-    };
+    });
 
 });
